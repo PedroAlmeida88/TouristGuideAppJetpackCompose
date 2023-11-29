d75322dfc624e12fe40cc4ab135996fa20f80fd9
José Pedro Sousa Almeida <80987531+PedroAlmeida88@users.noreply.github.com>
2023-11-29 19:25:03 +0000

    Added LocationScreen

---
+++ app/src/main/java/pt/isec/amovtp/touristapp/ui/screens/LocationsScreen.kt
@@ -0,0 +1,125 @@
+package pt.isec.amovtp.touristapp.ui.screens
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.clipToBounds
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.max
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.navigation.NavHostController
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory
+import org.osmdroid.util.GeoPoint
+import org.osmdroid.views.MapView
+import org.osmdroid.views.overlay.Marker
+import pt.isec.amovtp.touristapp.R
+import pt.isec.amovtp.touristapp.ui.viewmodels.LocationViewModel
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun LocationsScreen(modifier: Modifier = Modifier, navController: NavHostController?,viewModel : LocationViewModel) {
+    var autoEnabled by remember{ mutableStateOf(false) }
+    val location = viewModel.currentLocation.observeAsState()
+
+    var geoPoint by remember{ mutableStateOf(GeoPoint(
+        location.value?.latitude ?: 0.0, location.value?.longitude ?: 0.0
+    )) }
+    
+    Column(
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .fillMaxSize()
+
+            .padding(8.dp)
+    ) {
+        Row(
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.Top
+        ){
+            Button(
+                onClick = {  }
+            ) {
+                Text(text = "A...Z")
+            }
+            Button(
+                onClick = {  }
+            ) {
+                Text(text = "Perto de mim")
+            }
+        }
+        Spacer(Modifier.height(16.dp))
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+        ) {
+            items(viewModel.POIs) { poi ->
+                Card(
+                    modifier = Modifier
+                        //.fillMaxHeight(0,5f) // Use 50% of the screen height
+                        .height(324.dp)
+                        .fillMaxWidth()
+                        .padding(8.dp)
+                        .clip(shape = RoundedCornerShape(16.dp)),
+                    elevation = CardDefaults.cardElevation(4.dp),
+                    colors = CardDefaults.cardColors(
+                        containerColor = MaterialTheme.colorScheme.tertiary
+                    ),
+
+                    onClick = {
+                        //geoPoint = GeoPoint(poi.latitude, poi.longitude)
+                        navController?.navigate(Screens.POI.route)
+                    }
+                ) {
+                    Image(painter = painterResource(id = poi.imagesRes), contentDescription = "Barcelona")
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(8.dp),
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Text(text = poi.team, fontSize = 20.sp)
+                        Text(text = "${poi.latitude} ${poi.longitude}", fontSize = 14.sp)
+
+                    }
+                }
+            }
+        }
+
+    }
+
+}
