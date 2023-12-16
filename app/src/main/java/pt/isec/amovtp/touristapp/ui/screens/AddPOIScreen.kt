e10cff2c6cfff5bfde25d4469f5ad7d96066beb4
José Pedro Sousa Almeida <80987531+PedroAlmeida88@users.noreply.github.com>
2023-12-16 22:49:34 +0000

    Add POIs

---
+++ app/src/main/java/pt/isec/amovtp/touristapp/ui/screens/AddPOIScreen.kt
@@ -0,0 +1,251 @@
+package pt.isec.amovtp.touristapp.ui.screens
+
+import android.util.Log
+import android.widget.Toast
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CutCornerShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import pt.isec.amovtp.touristapp.data.Category
+import pt.isec.amovtp.touristapp.data.PointOfInterest
+import pt.isec.amovtp.touristapp.ui.composables.TakePhotoOrLoadFromGallery
+import pt.isec.amovtp.touristapp.ui.viewmodels.FirebaseViewModel
+import pt.isec.amovtp.touristapp.ui.viewmodels.LocationViewModel
+
+
+@Composable
+fun AddPOIScreen(modifier: Modifier.Companion, navController: NavHostController?,locationViewModel: LocationViewModel, firebaseViewModel: FirebaseViewModel) {
+    val context = LocalContext.current
+    val selectedLocation = locationViewModel.selectedLocation
+
+    var poiName by remember { mutableStateOf("") }
+    var poiDescription by remember { mutableStateOf("") }
+    var categorySelected by remember { mutableStateOf("") }
+    var longitude by remember { mutableStateOf("") }
+    var latitude by remember { mutableStateOf("") }
+
+    var isFormValid by remember { mutableStateOf(false) }
+    var isInputEnabled by remember { mutableStateOf(false) }
+    var isError by remember { mutableStateOf(false) }
+
+    //coordenadas
+    val location = locationViewModel.currentLocation.observeAsState()
+
+
+    fun validateForm() {
+        val longitudeDouble: Double? = longitude.toDoubleOrNull()
+        val latitudeDouble: Double? = latitude.toDoubleOrNull()
+
+        isFormValid = poiName.isNotBlank() &&
+                poiDescription.isNotBlank() &&
+                longitudeDouble != null &&
+                latitudeDouble != null &&
+                locationViewModel.imagePath.value != null
+
+    }
+
+    Column (
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(24.dp)
+            .verticalScroll(rememberScrollState())
+    ) {
+
+        if(isError)
+            Text(
+                text = "Preencher todos os campos (corretamente)",
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally),
+                color = Color.Red
+            )
+        Text(
+            text = "POI Name",
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+        )
+
+        OutlinedTextField(
+            value = poiName,
+            onValueChange ={
+                poiName = it
+                validateForm()
+            },
+            label = { Text(text = "POI Name") },
+
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(
+            text = "POI Description",
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+        )
+        OutlinedTextField(
+            value = poiDescription,
+            onValueChange ={
+                poiDescription = it
+                validateForm()
+            },
+            label = { Text(text = "Location Description") },
+        )
+        //TODO:Verificação da categoria, dropdown?
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(
+            text = "Category",
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+
+        )
+        OutlinedTextField(
+            value = categorySelected,
+            onValueChange ={
+                categorySelected = it
+                validateForm()
+            },
+            label = { Text(text = "Category") },
+        )
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(IntrinsicSize.Min)
+                .padding(8.dp)
+        ) {
+            Button(
+                onClick = {
+                    isInputEnabled = false;
+                    latitude = (location.value?.latitude ?: 0.0).toString()
+                    longitude = (location.value?.longitude ?: 0.0).toString()
+                    validateForm()
+
+                },
+                modifier = Modifier
+                    .weight(1f)
+                    .height(96.dp)
+                    .padding(end = 8.dp),
+                colors = ButtonDefaults.buttonColors(MaterialTheme.colorScheme.primary),
+                shape = CutCornerShape(percent = 0)
+                ) {
+                Text(text = "Get coordinates from current location")
+            }
+
+            Button(
+                onClick = {
+                    isInputEnabled = true
+                    validateForm()
+                },
+                modifier = Modifier
+                    .weight(1f)
+                    .height(96.dp)
+                    .padding(end = 8.dp),
+                colors = ButtonDefaults.buttonColors(MaterialTheme.colorScheme.primary),
+                shape = CutCornerShape(percent = 0)
+                ) {
+                Text(text = "Write coordinates")
+            }
+        }
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(IntrinsicSize.Min)
+                .padding(8.dp)
+        ) {
+            OutlinedTextField(
+                value = longitude,
+                onValueChange = {
+                    longitude = it
+                    validateForm()
+                },
+                label = { Text(text = "Longitude") },
+                enabled = isInputEnabled,
+                modifier = Modifier.weight(1f)
+            )
+            OutlinedTextField(
+                value = latitude,
+                onValueChange = {
+                    latitude = it
+                    validateForm()
+                },
+                label = { Text(text = "Latitude") },
+                enabled = isInputEnabled,
+                modifier = Modifier.weight(1f)
+
+            )
+        }
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .border(1.dp, Color.Gray, RoundedCornerShape(4.dp))
+                .padding(8.dp)
+                .weight(1f)
+        ) {
+            TakePhotoOrLoadFromGallery(locationViewModel.imagePath, Modifier.fillMaxSize())
+            validateForm()
+        }
+        Button(
+            onClick = {
+                if (!isFormValid) {
+                    isError = true
+                } else {
+                    isError = false
+                    var POI = PointOfInterest(
+                        name = poiName,
+                        description = poiDescription,
+                        category = Category("tetse",2,"",""),
+                        latitude = latitude.toDouble(),
+                        longitude = longitude.toDouble(),
+                        photoUrl = ""
+                    )
+                    Log.i("TAG", "AddPOIScreen: " + POI)
+
+                    firebaseViewModel.addPOIToFirestore(selectedLocation?.name ?: ""  ,POI)
+                    firebaseViewModel.uploadPOIToStorage(directory = "images/" + selectedLocation?.name +"/pois/",imageName = poiName, path = locationViewModel.imagePath.value ?: "", locationName = selectedLocation?.name ?: "" )
+                    navController?.popBackStack()
+                    Toast.makeText(context,"POI adicionada com sucesso!",Toast.LENGTH_LONG).show()
+                }
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(48.dp)
+                .padding(8.dp),
+            colors = ButtonDefaults.buttonColors(MaterialTheme.colorScheme.primary),
+        ) {
+            Text(text = "Submit")
+        }
+
+
+    }
+
+
+}
+
