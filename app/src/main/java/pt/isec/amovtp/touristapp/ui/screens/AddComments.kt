833affaf7f3ee9bc6192f0f98b16e3eb49072fdf
AcaCoutinho <coutinho.acacio.7@gmail.com>
2023-12-18 15:28:33 +0000

    AddCategory and AddComments inicio

---
+++ app/src/main/java/pt/isec/amovtp/touristapp/ui/screens/AddComments.kt
@@ -0,0 +1,10 @@
+package pt.isec.amovtp.touristapp.ui.screens
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import pt.isec.amovtp.touristapp.ui.viewmodels.FirebaseViewModel
+
+@Composable
+fun AddComments(modfier: Modifier.Companion, firebaseViewModel: FirebaseViewModel) {
+
+}
